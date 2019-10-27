3c9aa6733a9b12626b56b796fe96c0340fff8d27
Diego Almeida <diegalmeida@hotmail.com>
2019-10-27 17:29:55 -0300

    adicionando variaveis de ambiente

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 //pacote usado para se conectar com api
 import axios from 'axios';
 
-const api = axios.create({ baseURL : 'http://localhost:3001/api/'});
+const api = axios.create({ baseURL : process.env.REACT_APP_API_URL});
 
 export default api;
